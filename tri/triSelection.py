970040191bfbed3c948c740c9cc816df8bb2fd14
Arthur Voirin <59776043+arthurvoirin@users.noreply.github.com>
2021-01-21 08:43:25 +0100

    Update triSelection.py

--- tri/triSelection.py
+++ tri/triSelection.py
@@ -1,1 +1,24 @@
 
+L = [3,2,7,1,9,5,6,8,4] #Liste à trier
+
+#Définition de la fonction permettant le tri par sélection de L
+def triSelection(L):
+    
+    longueurL = len(L) #Longueur de la liste
+    
+    for enCours in range(0,longueurL):    
+        
+        plusPetit = enCours #Passage en revue de tous les éléments de la liste 1 à 1
+        
+        for i in range(enCours+1,longueurL): 
+            if L[i] < L[plusPetit]: #Comparaison de l'élément plusPetit (= avec l'index enCours) avec les suivants (i)
+                plusPetit = i #Si un élément est plus petit que celui enCours, alors plusPetit = i
+        
+        if min is not enCours: #Si le plus petit nombre de  la liste est != enCours
+            attendeDeplacement = L[enCours] #Mise dans attendeDeplacement du nombre enCours 
+            L[enCours] = L[plusPetit]
+            L[plusPetit] = attendeDeplacement #Déplacement du nombre enCours et inversion avec attenteDeplacement
+
+    return(L)
+
+print(triSelection(L)) #Affichage du résultat du tri après traitement
